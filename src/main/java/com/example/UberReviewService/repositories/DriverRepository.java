9797804ef1a183fd3158689cd99567ef53fb9c9c
Sarthak Bhandari <sarthakbhandari49@gmail.com>
2026-04-08 02:40:46 +0000

    Learned about fetch modes

---
+++ src/main/java/com/example/UberReviewService/repositories/DriverRepository.java
@@ -0,0 +1,11 @@
+package com.example.UberReviewService.repositories;
+
+import com.example.UberReviewService.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface DriverRepository extends JpaRepository<Driver,Long> {
+    Optional<Driver> findByIdAndLicenseNumber(Long id, String licenseNumber);
+}
--- src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,8 +1,11 @@
 package com.example.UberReviewService.services;
 
 import com.example.UberReviewService.models.Booking;
+import com.example.UberReviewService.models.BookingStatus;
+import com.example.UberReviewService.models.Driver;
 import com.example.UberReviewService.models.Review;
 import com.example.UberReviewService.repositories.BookingRepository;
+import com.example.UberReviewService.repositories.DriverRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
@@ -18,39 +21,72 @@ public class ReviewService  implements CommandLineRunner {
 
     private final BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository){
+    private final DriverRepository driverRepository;
+
+    public ReviewService(ReviewRepository reviewRepository,
+                         BookingRepository bookingRepository,
+                         DriverRepository driverRepository
+                         ){
         this.reviewRepository = reviewRepository;
         this.bookingRepository=bookingRepository;
+        this.driverRepository = driverRepository;
     }
 
     @Override
     public void run(String... args){
         System.out.println("**************");
-//        Review r = Review.builder()
-//                .content("Amazing ride quality")
-//                .rating(4.0)
-//                .build();
-//
-//        Booking b = Booking.builder()
-//                .review(r)
-//                .endTime(LocalDateTime.now())
-//                .build();
-//
-//
-//        bookingRepository.save(b);
-////
-//        List<Review> reviews = reviewRepository.findAll();
-//        for(Review review : reviews){
-//            System.out.println(review.getContent());
-//        }
 
-//        reviewRepository.deleteById(2L);
+        /*
+        Review r = Review.builder()
+                .content("Amazing ride quality")
+                .rating(4.0)
+                .build();
+
+        Booking b = Booking.builder()
+                .review(r)
+                .endTime(LocalDateTime.now())
+                .build();
+
+
+        bookingRepository.save(b);
+
+        List<Review> reviews = reviewRepository.findAll();
+        for(Review review : reviews){
+            System.out.println(review.getContent());
+        }
+
+        reviewRepository.deleteById(2L);
+
+         */
+
+        /*
 
         Optional<Booking> b =bookingRepository.findById(2L);
 
-//        if(b.isPresent()){
-//            bookingRepository.delete(b.get());
-//        }
+        if(b.isPresent()){
+            bookingRepository.delete(b.get());
+        }
         b.ifPresent(bookingRepository::delete);
+
+         */
+
+
+        /*
+        Optional<Driver> driver = driverRepository.findByIdAndLicenseNumber(1L,"UK074141");
+
+        driver.ifPresent(value -> {
+            System.out.println(value.getId());
+            List<Booking> bookings = bookingRepository.findAllByDriverId(1L);
+            for(Booking booking :bookings){
+                System.out.println(booking.getDriver()+" "+booking.getId() + " " + booking.getReview().getContent());
+            }
+        });
+
+         */
+
+        Optional<Driver> driver = driverRepository.findById(1L); // FetchType is EAGER
+
+        driver.ifPresent(value -> System.out.println(value.getId()));
+
     }
 }
